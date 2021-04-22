dd4f6a806ca5e13d56f5c03381dddae57541fd19
Pedro Lima <pedro.lima.wk@gmail.com>
2021-04-22 19:48:03 -0300

    feat: Creating routes to list and create messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,23 @@
 import { response, Router } from "express";
+import { MessagesController } from "./controllers/MessagesController";
 import { SettingsController } from "./controllers/SettingsController";
 import { UsersController } from "./controllers/UsersController";
 
 const routes = Router();
 
 const settingsController = new SettingsController();
-const usersController = new UsersController()
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 
+//Settings routes
 routes.post("/settings", settingsController.create);
 
-routes.post("/users", usersController.create)
+//Users routes
+routes.post("/users", usersController.create);
 
-export {routes};
+//Messages routes
+routes.post("/messages", messagesController.create);
+routes.get("/messages/:id", messagesController.showByUser);
+
+
+export { routes };
