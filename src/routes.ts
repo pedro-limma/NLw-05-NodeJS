1485b5b9e728438844fb82cdd3964ffb4694be03
Pedro Lima <pedro.lima.wk@gmail.com>
2021-04-25 16:10:33 -0300

    chore: Update on the routes for testing the websocket

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { response, Router } from "express";
 import { MessagesController } from "./controllers/MessagesController";
 import { SettingsController } from "./controllers/SettingsController";
 import { UsersController } from "./controllers/UsersController";
+import { SettingsService } from "./services/SettingsService";
 
 const routes = Router();
 
@@ -11,6 +12,8 @@ const messagesController = new MessagesController();
 
 //Settings routes
 routes.post("/settings", settingsController.create);
+routes.get("/settings/:username", settingsController.findByUsername);
+routes.put("/settings/:username", settingsController.update);
 
 //Users routes
 routes.post("/users", usersController.create);
